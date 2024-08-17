dd4f39416245e4537f5a97f8a8123961752fb431
Gabriel Silva <gabrielssilva3107@gmail.com>
2024-08-16 21:11:53 -0300

    Alterando data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 21, 2023 19:00:00");
+const dataDoEvento = new Date("Sep 7, 2024 00:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function () {
